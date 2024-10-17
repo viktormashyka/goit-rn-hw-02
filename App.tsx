e10ec587f2917bd4fd8c88db5b1e2a8b2ae06519
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2024-10-17 21:08:41 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { colors } from "./styles/global";
+import RegistrationScreen from "./screens/RegistrationScreen";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+  if (!fontsLoaded) {
+    <ActivityIndicator color={colors.orange} />;
+  }
+  return (
+    <>
+      <RegistrationScreen />
+      <StatusBar style="auto" />
+    </>
+  );
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# goit-rn-hw-01
+
+Встановіть глобально expo cli: npm install -g expo-cli
+
+Створіть пустий шаблон expo додатку expo init MyNewApp
+
+Встановіть бібліотеку для шрифтів npx expo install expo-font
