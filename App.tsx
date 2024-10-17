d37303dcd7bd119825340c22283a9e609f161fa9
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2024-10-17 22:20:10 +0100

    fix formContainer height

--- App.tsx
+++ App.tsx
@@ -18,8 +18,8 @@ export default function App() {
   }
   return (
     <>
-      <RegistrationScreen />
-      {/* <LoginScreen /> */}
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
       <StatusBar style="auto" />
     </>
   );
